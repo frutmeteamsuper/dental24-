d6d8d40dd7764d657a2c63f4ccdf9d1a1579fe39
root <root@iPhone>
2021-07-06 22:53:05 -0400

    testapp

--- src/app/components/testapp/testapp.component.ts
+++ src/app/components/testapp/testapp.component.ts
@@ -18,9 +18,11 @@ export class TestappComponent implements OnInit {
      ) { }
    loadAPI = null;  
 
-  url = "assets/assetsdental/js/slick.js";
-  url2 = "assets/assetsdental/plugins/swiper/js/swiper.min.js";
+  url = "assets/assetsdental/js/jquery.min.js";
+  url1 = "assets/assetsdental/js/popper.min.js";
+  url2= "assets/assetsdental/js/slick.js";
   url3 = "assets/assetsdental/plugins/swiper/js/swiper.min.js";
+  url4 = "assets/assetsdental/js/script.js";
 
  public loadScript() {
     let node = document.createElement("script");
@@ -30,6 +32,15 @@ export class TestappComponent implements OnInit {
     node.charset = "utf-8";
     document.getElementsByTagName("head")[0].appendChild(node);
   }
+
+  public loadScript1() {
+    let node = document.createElement("script");
+    node.src = this.url1;
+    node.type = "text/javascript";
+    node.async = true;
+    node.charset = "utf-8";
+    document.getElementsByTagName("head")[0].appendChild(node);
+  }   
   public loadScript2() {
     let node = document.createElement("script");
     node.src = this.url2;
@@ -41,6 +52,14 @@ export class TestappComponent implements OnInit {
    public loadScript3() {
     let node = document.createElement("script");
     node.src = this.url3;
+    node.type = "text/javascript";
+    node.async = true;
+    node.charset = "utf-8";
+    document.getElementsByTagName("head")[0].appendChild(node);
+  } 
+   public loadScript4() {
+    let node = document.createElement("script");
+    node.src = this.url4;
     node.type = "text/javascript";
     node.async = true;
     node.charset = "utf-8";
@@ -147,8 +166,10 @@ cartCalculate(){
  if (this._uw.loaded==true){
       this.loadAPI = new Promise(resolve => {
         this.loadScript();
+        this.loadScript1();
         this.loadScript2();
         this.loadScript3();
+        this.loadScript4();
         // this.loadScript3();
         });
       }
